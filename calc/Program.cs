e7f741f9740c03cefc3802141fe34df727726d98
Artur aerty <ikonnikov.artur@mail.ru>
2021-06-05 12:36:43 +0700

    MINUS

--- calc/Program.cs
+++ calc/Program.cs
@@ -6,7 +6,22 @@ namespace calc
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Введите первое число");
+            int A = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            int B = int.Parse(Console.ReadLine());
+            Console.WriteLine("Выберите действие: \n1) +\n2) -\n3) *\n4) /");
+            int vari = int.Parse(Console.ReadLine());
+            switch (vari)
+            {
+                case 2:
+                    minus(A, B);
+                    break;
+            }
+            void minus(int a, int b)
+            {
+                Console.WriteLine(a - b);
+            }
         }
     }
 }
