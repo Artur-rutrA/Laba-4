3335d5fa801abd7d427ee64faafcef1da6fcf694
Artur aerty <ikonnikov.artur@mail.ru>
2021-06-05 12:43:58 +0700

    DEVIDE

--- calc/Program.cs
+++ calc/Program.cs
@@ -6,7 +6,24 @@ namespace calc
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Введите первое число");
+            int A = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            int B = int.Parse(Console.ReadLine());
+            Console.WriteLine("Выберите действие: \n1) +\n2) -\n3) *\n4) /");
+            int vari = int.Parse(Console.ReadLine());
+            switch (vari)
+            {
+                case 4:
+                    devide(A, B);
+                    break;
+            }
+            void devide(int a, int b)
+            {
+                double _1 = (double)a;
+                double _2 = (double)b;
+                Console.WriteLine(_1 / _2);
+            }
         }
     }
 }
